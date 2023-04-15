ec7b313578da5f0d7a2e084a27b7bbd30da1e51c
shivam <shivam221098@gmail.com>
2023-04-15 23:48:23 +0530

    urls routed

--- storage/views.py
+++ storage/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+
+def home(request: HttpRequest):
+    return HttpResponse("<a>Hello!<a>")
