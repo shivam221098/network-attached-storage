dff61d613327726c78ee38af41b61b3a039629ff
shivam <shivam221098@gmail.com>
2023-04-15 22:02:08 +0530

    Initial commit

---
+++ network_attached_storage/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for network_attached_storage project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "network_attached_storage.settings")
+
+application = get_asgi_application()
