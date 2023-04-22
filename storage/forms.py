f47143e4d276deb8449918116e0b0ad07ed6848d
shivam <shivam221098@gmail.com>
2023-04-22 19:01:00 +0530

    Added basic signup functionality

--- storage/forms.py
+++ storage/forms.py
@@ -8,3 +8,14 @@ class SignUpForm(UserCreationForm):
         fields = (
             "first_name", "last_name", "email", "username", "password1", "password2"
         )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].required = True
+
+        # remove autofocus from username
+        self.fields['username'].widget.attrs.pop("autofocus", None)
+
+        # add autofocus in first_name
+        self.fields['first_name'].widget.attrs.update({"autofocus": True})
