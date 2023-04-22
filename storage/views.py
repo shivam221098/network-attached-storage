ab0ea8ae7348cc0e6eb3e775f4c25feb7ebc3b10
shivam <shivam221098@gmail.com>
2023-04-22 21:18:38 +0530

    can create directory for newly added user

--- storage/views.py
+++ storage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpRequest
 from django.contrib.auth import authenticate, login, logout
 from django.views import generic
 from django.urls import reverse_lazy
