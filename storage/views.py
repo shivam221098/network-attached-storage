630465b3ad0b58085079213bb47a1e69bb686799
shivam <shivam221098@gmail.com>
2023-04-22 21:18:15 +0530

    can create directory for newly added user

--- storage/views.py
+++ storage/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse, HttpRequest
 from django.contrib.auth import authenticate, login, logout
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.conf import settings
 from .forms import SignUpForm
+from .models import StorageUser
 import os
 
 
@@ -11,6 +14,32 @@ class SignUpView(generic.CreateView):
     form_class = SignUpForm
     template_name = "storage/storage-signup.html"
     success_url = reverse_lazy("storage_home")
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("/dashboard")
+        return super(SignUpView, self).get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        response = super(SignUpView, self).form_valid(form)
+
+        username = form.cleaned_data['username']
+        user = User.objects.get(username=username)
+
+        if user:
+            storage_user = StorageUser.objects.create(username=user)
+            storage_user.save()
+
+        dir_obj = StorageUser.objects.get(username=user)
+        if dir_obj:
+            dir_name = dir_obj.dir_uuid.urn[9:]
+            path = settings.FILE_PATH_DIRECTORY / dir_name
+
+            # check if directory exists
+            if not os.path.exists(path):
+                os.mkdir(path)
+
+        return response
 
 
 def home(request: HttpRequest):
@@ -42,6 +71,9 @@ def logout_user(request: HttpRequest):
     if request.user.is_authenticated:
         logout(request)
     return redirect("/")
+
+
+# def create_user_storage_profile(request: HttpRequest):
 
 
 def dashboard(request: HttpRequest):
